61b55500e4d01060292d4a9c16c14b0027e269e6
dAppBridge <38129939+dAppBridge@users.noreply.github.com>
2018-05-18 14:06:23 +0100

    Update dAppBridge-Client_Rinkeby.sol

--- dAppBridge-Client_Rinkeby.sol
+++ dAppBridge-Client_Rinkeby.sol
@@ -132,42 +132,42 @@ contract clientOfdAppBridge {
     mapping(string => bytes32) internal callback_content_verifications_int;
 
     function setTimeout(string callback_method, uint32 timeout) internal dAppBridgeClient {
-        uint256 _reward = dAppBridge.getMinReward('setTimeout');//+user_callback_gas;
+        uint256 _reward = dAppBridge.getMinReward('setTimeout')+user_callback_gas;
         dAppBridge.setTimeout.value(_reward).gas(current_gas)(callback_method, timeout);
         //emit newEventResponse(_new_request_key);
         //callback_content_verifications[_new_request_key] = 0x00;
     }
     function setURLTimeout(string callback_method, uint32 timeout, string external_url, string external_params) internal dAppBridgeClient {
-        uint256 _reward = dAppBridge.getMinReward('setURLTimeout');//+user_callback_gas;
+        uint256 _reward = dAppBridge.getMinReward('setURLTimeout')+user_callback_gas;
         dAppBridge.setURLTimeout.value(_reward).gas(current_gas)(callback_method, timeout, external_url, external_params, "");
         //emit newEventResponse(_new_request_key);
         //callback_content_verifications[_new_request_key] = 0x00;
     }
     function setURLTimeout(string callback_method, uint32 timeout, string external_url, string external_params, string json_extract_element) internal dAppBridgeClient {
-        uint256 _reward = dAppBridge.getMinReward('setURLTimeout');//+user_callback_gas;
+        uint256 _reward = dAppBridge.getMinReward('setURLTimeout')+user_callback_gas;
         dAppBridge.setURLTimeout.value(_reward).gas(current_gas)(callback_method, timeout, external_url, external_params, json_extract_element);
         //emit newEventResponse(_new_request_key);
         //callback_content_verifications[_new_request_key] = 0x00;
     }
     function callURL(string callback_method, string external_url, string external_params) internal dAppBridgeClient {
-        uint256 _reward = dAppBridge.getMinReward('callURL');//+user_callback_gas;
+        uint256 _reward = dAppBridge.getMinReward('callURL')+user_callback_gas;
         dAppBridge.callURL.value(_reward).gas(current_gas)(callback_method, external_url, external_params, "");
         //emit newEventResponse(_new_request_key);
         //callback_content_verifications[_new_request_key] = 0x00;
     }
     function callURL(string callback_method, string external_url, string external_params, string json_extract_elemen) internal dAppBridgeClient {
-        uint256 _reward = dAppBridge.getMinReward('callURL');//+user_callback_gas;
+        uint256 _reward = dAppBridge.getMinReward('callURL')+user_callback_gas;
         dAppBridge.callURL.value(_reward).gas(current_gas)(callback_method, external_url, external_params, json_extract_elemen);
         //emit newEventResponse(_new_request_key);
         //callback_content_verifications[_new_request_key] = 0x00;
     }
     function randomNumber(string callback_method, int32 min_val, int32 max_val, uint32 timeout) internal dAppBridgeClient {
-        uint256 _reward = dAppBridge.getMinReward('randomNumber');//+user_callback_gas;
+        uint256 _reward = dAppBridge.getMinReward('randomNumber')+user_callback_gas;
         dAppBridge.randomNumber.value(_reward).gas(current_gas)(callback_method, min_val, max_val, timeout);
         //emit newEventResponse(_new_request_key);
     }
     function randomString(string callback_method, uint8 number_of_bytes, uint32 timeout) internal dAppBridgeClient {
-        uint256 _reward = dAppBridge.getMinReward('randomString');//+user_callback_gas;
+        uint256 _reward = dAppBridge.getMinReward('randomString')+user_callback_gas;
         dAppBridge.randomString.value(_reward).gas(current_gas)(callback_method, number_of_bytes, timeout);
         //emit newEventResponse(_new_request_key);
         //callback_content_verifications[_new_request_key] = 0x00;
