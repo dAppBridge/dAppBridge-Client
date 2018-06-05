311b60ea59e881cfa2f12cd9ad813e5eec321e78
dAppBridge <38129939+dAppBridge@users.noreply.github.com>
2018-06-05 09:44:24 +0100

    Update dAppBridge-Client_Kovan.sol

--- dAppBridge-Client_Kovan.sol
+++ dAppBridge-Client_Kovan.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.16;
 
 interface dAppBridge_I {
+    function getOwner() external returns(address);
     function getMinReward(string requestType) external returns(uint256);
     function getMinGas() external returns(uint256);    
     function setTimeout(string callback_method, uint32 timeout) external payable;
@@ -58,7 +59,7 @@ contract clientOfdAppBridge {
         address checkAddress
     );
     
-    modifier only_dAppBridge {
+    modifier only_dAppBridge_ {
         initBridge();
         
         //emit event_senderAddress(msg.sender);
@@ -67,47 +68,16 @@ contract clientOfdAppBridge {
         require(msg.sender == address(dAppBridge));
         _;
     }
+
+    // Ensures that only the dAppBridge system can call the function
+    modifier only_dAppBridge {
+        initBridge();
+        address _dAppBridgeOwner = dAppBridge.getOwner();
+        require(msg.sender == _dAppBridgeOwner);
+
+        _;
+    }
     
-    event verifyProofEvent(
-        string requestKey,
-        string inData,
-        string verificationString,
-        string inDataProofPreConcat,
-        string inDataProof,
-        bool compareResult
-    );
-
-    modifier verifyProof(string requestKey, string inData) {
-        string memory inDataProof =  concatStrings("0x", bytes32string(bytes32(keccak256(inData))));
-        
-        emit verifyProofEvent(requestKey, inData, 
-        callback_content_verifications[requestKey],
-        bytes32string(bytes32(keccak256(inData))),
-        inDataProof,
-        compareStrings(callback_content_verifications[requestKey], inDataProof));
-        
-        require(
-            compareStrings(callback_content_verifications[requestKey], inDataProof) == true
-            );
-
-        _;
-    }
-    event verifyProofEventInt(
-        string requestKey,
-        int inData,
-        bytes32 verifyProof,
-        bytes32 inDataProof
-    );
-    modifier verifyIntProof(string requestKey, int inData) {
-        emit verifyProofEventInt(requestKey, inData, callback_content_verifications_int[requestKey],
-        bytes32(keccak256(inData)));
-        
-        require(
-            callback_content_verifications_int[requestKey] == bytes32(keccak256(inData))
-            );
-        
-        _;
-    }
 
     
     function setGas(uint256 new_gas) internal {
@@ -120,72 +90,39 @@ contract clientOfdAppBridge {
         user_callback_gas = new_callback_gas;
     }
 
-    // requestKey secure as only DappBridge can post to it
-    /*
-    event newEventResponse(
-        bytes32 requestKey
-    );
-    */
     
-    // requestKey=>content md5 hash (Only DappBridge able to post into this collection)
-    mapping(string => string) internal callback_content_verifications;
-    mapping(string => bytes32) internal callback_content_verifications_int;
 
     function setTimeout(string callback_method, uint32 timeout) internal dAppBridgeClient {
         uint256 _reward = dAppBridge.getMinReward('setTimeout')+user_callback_gas;
         dAppBridge.setTimeout.value(_reward).gas(current_gas)(callback_method, timeout);
-        //emit newEventResponse(_new_request_key);
-        //callback_content_verifications[_new_request_key] = 0x00;
+
     }
     function setURLTimeout(string callback_method, uint32 timeout, string external_url, string external_params) internal dAppBridgeClient {
         uint256 _reward = dAppBridge.getMinReward('setURLTimeout')+user_callback_gas;
         dAppBridge.setURLTimeout.value(_reward).gas(current_gas)(callback_method, timeout, external_url, external_params, "");
-        //emit newEventResponse(_new_request_key);
-        //callback_content_verifications[_new_request_key] = 0x00;
+
     }
     function setURLTimeout(string callback_method, uint32 timeout, string external_url, string external_params, string json_extract_element) internal dAppBridgeClient {
         uint256 _reward = dAppBridge.getMinReward('setURLTimeout')+user_callback_gas;
         dAppBridge.setURLTimeout.value(_reward).gas(current_gas)(callback_method, timeout, external_url, external_params, json_extract_element);
-        //emit newEventResponse(_new_request_key);
-        //callback_content_verifications[_new_request_key] = 0x00;
     }
     function callURL(string callback_method, string external_url, string external_params) internal dAppBridgeClient {
         uint256 _reward = dAppBridge.getMinReward('callURL')+user_callback_gas;
         dAppBridge.callURL.value(_reward).gas(current_gas)(callback_method, external_url, external_params, "");
-        //emit newEventResponse(_new_request_key);
-        //callback_content_verifications[_new_request_key] = 0x00;
     }
     function callURL(string callback_method, string external_url, string external_params, string json_extract_elemen) internal dAppBridgeClient {
         uint256 _reward = dAppBridge.getMinReward('callURL')+user_callback_gas;
         dAppBridge.callURL.value(_reward).gas(current_gas)(callback_method, external_url, external_params, json_extract_elemen);
-        //emit newEventResponse(_new_request_key);
-        //callback_content_verifications[_new_request_key] = 0x00;
     }
     function randomNumber(string callback_method, int32 min_val, int32 max_val, uint32 timeout) internal dAppBridgeClient {
         uint256 _reward = dAppBridge.getMinReward('randomNumber')+user_callback_gas;
         dAppBridge.randomNumber.value(_reward).gas(current_gas)(callback_method, min_val, max_val, timeout);
-        //emit newEventResponse(_new_request_key);
     }
     function randomString(string callback_method, uint8 number_of_bytes, uint32 timeout) internal dAppBridgeClient {
         uint256 _reward = dAppBridge.getMinReward('randomString')+user_callback_gas;
         dAppBridge.randomString.value(_reward).gas(current_gas)(callback_method, number_of_bytes, timeout);
-        //emit newEventResponse(_new_request_key);
-        //callback_content_verifications[_new_request_key] = 0x00;
     }
     
-
-    event newProofEvent(
-        string requestKey,
-        string proof
-    );
-
-    function receiveProof(string requestkey, string proof) external only_dAppBridge {
-        emit newProofEvent(requestkey, proof);
-        callback_content_verifications[requestkey] = proof;
-    }
-    function receiveProofInt(string requestkey, int proof) external only_dAppBridge {
-        callback_content_verifications_int[requestkey] = bytes32(keccak256(proof));
-    }
 
     // Helper internal functions
     function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
