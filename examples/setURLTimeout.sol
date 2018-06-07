e01388bc34197626f50489edcd5b97fd80a411c8
dAppBridge - bringing smart contracts alive <38129939+dAppBridge@users.noreply.github.com>
2018-06-07 17:30:55 +0100

    Update setURLTimeout.sol

--- examples/setURLTimeout.sol
+++ examples/setURLTimeout.sol
@@ -3,19 +3,18 @@ pragma solidity ^0.4.16;
 import "github.com/dAppBridge/dAppBridge-Client/dAppBridge-Client_Kovan.sol";
 
 //
-// Simple contract which gets the BTC/USD rate from Coindesk every 240 seconds
+// Simple contract which gets a random number between 1 and 100
 // Using the dAppBridge.com service
 // Start by calling startTesting
-// Contract will then run itself every 240 seconds
 //
 
-contract dAppBridgeTester_setURLTimeout is clientOfdAppBridge {
+contract DappBridgeTester_randomNumber is clientOfdAppBridge {
     
-    string public usd_btc_rate = "";
+    int256 public random_number = 0;
 
     // Contract Setup
     address public owner;
-    function dAppBridgeTester_setURLTimeout() payable {
+    function DappBridgeTester_randomNumber() payable {
         owner = msg.sender;
     }
     function kill() public {
@@ -24,15 +23,14 @@ contract dAppBridgeTester_setURLTimeout is clientOfdAppBridge {
     //
     
     // The key returned here can be matched up to the original response below
-    function callback(bytes32 key, string callbackData) external payable only_dAppBridge {
-        usd_btc_rate = callbackData;
-        bytes32 newkey = setURLTimeout("callback", 240, "https://api.coindesk.com/v1/bpi/currentprice.json", "", "bpi.USD.rate");
+    function callback(bytes32 key, int callbackData) external payable only_dAppBridge {
+        random_number = callbackData;
     }
     
 
     function startTesting() public {
     	if(msg.sender == owner)
-        	bytes32 newkey = setURLTimeout("callback", 0, "https://api.coindesk.com/v1/bpi/currentprice.json", "", "bpi.USD.rate");
+        	bytes32 newkey = randomNumber("callback", 1, 100, 0);
     }
 
 
